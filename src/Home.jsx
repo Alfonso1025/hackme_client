79fcc2d4abd9c929d39ed78d5b61e65392062f24
Alfonso1025 <alfonso25elorriaga@gmail.com>
2023-12-29 01:53:57 -0600

    added a hedaer in the home component

--- src/Home.jsx
+++ src/Home.jsx
@@ -11,7 +11,8 @@ function Home(props){
        
         
         <h1>Hackme if you can</h1>
-        
+        <h3>This website was built by alfonso-softtech to train staff on pentesting techniques</h3>
+        <h3>Only authorized personel is authorized to pentest this system.</h3>
     </div>
     
    
